b96aeb0f52abe107ac6726f69e76515f3485bca5
Simon Ndege <simopetley@gmail.com>
2025-07-09 17:33:33 +0300

    add changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
-import React from "react";
-import { image } from "../data/data";
-
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about" style={{ textAlign: "center", marginTop: "40px" }}>
+      <h2>About Me</h2>
+      <p>I made this</p>
+      <img
+        src="/images/demo.png" // NOTE: this will only work if the server exposes /images/
+        alt="I made this"
+        style={{ width: "300px", marginTop: "20px" }}
+      />
+    </div>
+  );
 }
-
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,13 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const headingText = `${name} is a Web Developer from ${city}`;
+  
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>{headingText}</h1>
+    </div>
+  );
 }
 
 export default Home;
